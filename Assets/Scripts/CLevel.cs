0d9b5f2a403986ea79fe7ff7add6cbd7a9fad4fa
Elias Pöyliö <elias.poylio@gmail.com>
2020-11-01 10:02:11 +0200

    .gitignore

---
+++ Assets/Scripts/CLevel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+[System.Serializable]
+public class CLevel
+{
+    public XArray[] yArray;
+}
